3a96ab47b9c8166b5939fe054a6baf8704a31261
jeffmayn <jeff@gyldenbrand.dk>
2023-01-10 00:02:30 +0100

    updated database config to heroku

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,20 @@
 import mysql from "mysql2";
 
 // create connection
+/*
 const db = mysql.createConnection({
    host: "localhost",
    user: "root",
    password: "",
    database: "mydb"
+});
+*/
+
+const db = mysql.createConnection({
+   host: "eu-cdbr-west-03.cleardb.net",
+   user: "ba3a97cc9ab551",
+   password: "68060962",
+   database: "heroku_d089aa07e0c2400"
 });
 
 export default db;
