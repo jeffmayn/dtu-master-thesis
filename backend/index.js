e94150f4db58539adbf61a9140638ab16ffe407f
jeffmayn <jeff@gyldenbrand.dk>
2023-01-15 22:42:57 +0100

    live

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ else {
 
 // to avoid crosssite-scripting etc on production build
 var corsOption = {
-   origin: "https://dtu-master-thesis-tir.herokuapp.com",
+   origin: "https://dtu-master-thesis-tir.herokuapp.com/components",
    optionSuccessStatus: 200
 };
  
