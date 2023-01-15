4f864fa635760b89b483e016396ba8dfe13df33e
jeffmayn <jeff@gyldenbrand.dk>
2023-01-15 23:17:37 +0100

    live

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -5,6 +5,7 @@ import {
    createComponent,
    deleteComponent,
 } from "../controllers/component.js";
+import cors from "cors";
 
 import {
    searchSpecificCVE,
@@ -13,16 +14,24 @@ import {
 } from "../controllers/cve.js";
 import { modelJson } from "../controllers/model.js";
 
+var corsOption = {
+   origin: [
+      "https://dtu-tir.netlify.app",
+      "https://dtu-master-thesis-tir.herokuapp.com",
+   ],
+   optionSuccessStatus: 200
+};
+
 // handles the rest endpoints from the frontend
 const router = express.Router();
-router.get("/");
-router.get("/components", showComponents);
-router.get("/components:id", showComponentById);
-router.post("/components", createComponent);
-router.post("/model", modelJson);
-router.delete("/delete/:id", deleteComponent);
-router.get("/cve/:id", searchSpecificCVE);
-router.post("/cve", searchCVEMentioningKeywords);
-router.post("/searchProduct", searchProducts);
+router.get("/", cors(corsOptions));
+router.get("/components", showComponents, cors(corsOptions));
+router.get("/components:id", showComponentById, cors(corsOptions));
+router.post("/components", createComponent, cors(corsOptions));
+router.post("/model", modelJson, cors(corsOptions));
+router.delete("/delete/:id", deleteComponent, cors(corsOptions));
+router.get("/cve/:id", searchSpecificCVE, cors(corsOptions));
+router.post("/cve", searchCVEMentioningKeywords, cors(corsOptions));
+router.post("/searchProduct", searchProducts, cors(corsOptions));
 
 export default router;
