e3a13d616c04d507a2f28f35f36c4d3740b8300c
jeffmayn <jeff@gyldenbrand.dk>
2023-01-15 23:02:11 +0100

    live

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ else {
 
 // to avoid crosssite-scripting etc on production build
 var corsOption = {
-   origin: "https://dtu-tir.netlify.app",
+   origin: ["https://dtu-tir.netlify.app", "https://dtu-master-thesis-tir.herokuapp.com"],
    optionSuccessStatus: 200
 };
  
