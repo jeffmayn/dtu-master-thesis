03a89512b6e9c41e7a3177eee16a67c3bbe13b80
jeffmayn <jeff@gyldenbrand.dk>
2023-01-16 01:14:17 +0100

    commit to test on prod

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,12 +6,54 @@ dotenv.config();
    creates the connection to either a local or external database
    based on the settings in the configuration-file
 */
+
+
+/*
 const db = mysql.createConnection({
    host: process.env.MODE == "dev" ? "localhost" : process.env.DB_HOST,
    user: process.env.MODE == "dev" ? "root" : process.env.DB_USER,
    password: process.env.MODE == "dev" ? "" : process.env.DB_PASSWORD,
    database: process.env.MODE == "dev" ? "mydb" : process.env.DB_NAME
 });
+*/
+
+
+const db = mysql.createConnection({
+   host: process.env.DB_HOST,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME
+});
+
+/*
+ 
+ var connection;
+ 
+ function handleDisconnect() {
+   connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                   // the old one cannot be reused.
+ 
+   connection.connect(function(err) {              // The server is either down
+     if(err) {                                     // or restarting (takes a while sometimes).
+       console.log('error when connecting to db:', err);
+       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+     }                                     // to avoid a hot loop, and to allow our node script to
+   });                                     // process asynchronous requests in the meantime.
+                                           // If you're also serving http, display a 503 error.
+   connection.on('error', function(err) {
+     console.log('db error', err);
+     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+       handleDisconnect();                         // lost due to either server restart, or a
+     } else {                                      // connnection idle timeout (the wait_timeout
+       throw err;                                  // server variable configures this)
+     }
+   });
+ }
+ 
+handleDisconnect();
+ */
+
+
    
 export default db;
 
