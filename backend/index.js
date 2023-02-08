7c2d4c7802ba42619c44528b5e21b1585555dca9
jeffmayn <jeff@gyldenbrand.dk>
2023-02-08 22:25:43 +0100

    live test

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.options("/*", function (req, res, next) {
    console.log("Got an options request yo");
    res.header('Access-Control-Allow-Origin', '*');
    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, XMLHttpRequest');
    res.sendStatus(200);
 });
 
